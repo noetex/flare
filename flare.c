a4e22c3aec06d875ee1905b975f5d1a8f23c5f2e
Rami Abbas <145647517+noetex@users.noreply.github.com>
2023-10-01 18:22:45 +0300

    move C libraries into C compilation unit

--- flare.c
+++ flare.c
@@ -2,8 +2,8 @@
 	#define NOMINMAX
 	#define WIN32_LEAN_AND_MEAN
 	#include<windows.h>
-	#include"flare_windows.h"
-	#include"flare_windows_resource.h"
+	#include"src/windows.h"
+	#include"src/windows_resource.h"
 #elif defined(FLARE_SYSTEM_LINUX) || defined(FLARE_SYSTEM_MACOS)
 	#include<unistd.h>
 	#include<termios.h>
@@ -28,13 +28,23 @@
 #define GLEW_STATIC
 #include"3rd_party/glew-2.2.0/include/GL/glew.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include"3rd_party/stb/stb_image.h"
+
+#define NUKLEAR_IMPLEMENTATION
+#include"3rd_party/nuklear/nuklear.h"
+
+#define MINIAUDIO_IMPLEMENTATION
+#include"3rd_party/miniaudio/miniaudio.h"
+
+
 #include"flare.h"
 
 #ifdef FLARE_SYSTEM_WINDOWS
 	#include"3rd_party/rpvgi/rpvgi_windows_commons.c"
-	#include"flare_windows_thread_sound.c"
-	#include"flare_windows_thread_renderer.c"
-	#include"flare_windows_main.c"
+	#include"windows_thread_sound.c"
+	#include"windows_thread_renderer.c"
+	#include"windows_thread_main.c"
 #elif defined FLARE_SYSTEM_LINUX
 	#include"flare_linux_main.c"
 #elif defined FLARE_SYSTEM_MACOS
--- flare.cpp
+++ flare.cpp
@@ -6,16 +6,6 @@
 	#error unspecified or unsupported operating system
 #endif
 
-#define STB_IMAGE_IMPLEMENTATION
-#include"3rd_party/stb/stb_image.h"
-
-#define NUKLEAR_IMPLEMENTATION
-#include"3rd_party/nuklear/nuklear.h"
-
-#define MINIAUDIO_IMPLEMENTATION
-#include"3rd_party/miniaudio/miniaudio.h"
-
-
 #include"flare.h"
 
 #ifdef FLARE_SYSTEM_WINDOWS
